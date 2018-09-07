015a29ce7251475693bc48a3d35d8deee4e71063
Jeliozver <jelioradoev@gmail.com>
2018-09-07 10:55:59 +0300

    added solution for Combinations without repetition

---
+++ P05-Combinations-Without-Repetition/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace P05_Combinations_Without_Repetition
+{
+    using System;
+
+    public class StartUp
+    {
+        private static int[] _loops;
+
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            _loops = new int[k];
+            CombinationsWithoutRepetition(n, k, 0, 1);
+        }
+
+        public static void CombinationsWithoutRepetition(int n, int k, int current, int loopStart)
+        {
+            if (current == k)
+            {
+                Print();
+                return;
+            }
+
+            for (int i = loopStart; i <= n; i++)
+            {
+                _loops[current] = i;
+                CombinationsWithoutRepetition(n, k, current + 1, i + 1);
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", _loops));
+        }
+    }
+}
