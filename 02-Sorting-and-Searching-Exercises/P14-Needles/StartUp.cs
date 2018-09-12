28673005378e166558d5e7a2a6c751c1c7b8e22b
Jeliozver <jelioradoev@gmail.com>
2018-09-12 12:19:47 +0300

    added solutions for Problem 14 Needles

---
+++ 02-Sorting-and-Searching-Exercises/P14-Needles/StartUp.cs
@@ -0,0 +1,83 @@
+﻿namespace P14_Needles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        private static int[] _elements;
+        private static int[] _needles;
+        private static List<int> _indexes = new List<int>();
+
+        public static void Main()
+        {
+            Console.ReadLine();
+
+            _elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            _needles = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            FindIndex(_elements, _needles, _indexes);
+            PrintResult();
+        }
+
+        private static void FindIndex(int[] elements, int[] needles, List<int> indexes)
+        {
+            int prev = elements[elements.Length - 1];
+
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                if (elements[i] == 0)
+                {
+                    elements[i] = prev;
+                }
+
+                prev = elements[i];
+            }
+
+            for (int i = 0; i < needles.Length; i++)
+            {
+                bool isInserted = false;
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (needles[i] <= elements[j])
+                    {
+                        indexes.Add(j);
+                        isInserted = true;
+                        break;
+                    }
+                }
+
+                if (!isInserted)
+                {
+                    int index = elements.Length - 1;
+
+                    while (index > 0 && elements[index] == 0)
+                    {
+                        index--;
+                    }
+
+                    if (elements[index] == 0)
+                    {
+                        index--;
+                    }
+
+                    _indexes.Add(index + 1);
+                }
+            }
+        }
+
+        private static void PrintResult()
+        {
+            Console.WriteLine(string.Join(" ", _indexes));
+        }
+    }
+}
